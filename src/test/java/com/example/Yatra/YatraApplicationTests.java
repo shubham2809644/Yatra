31afb904c7e495c9ce7c2b418114968d251aafe8
shubham bhople <shubham932000bhople@gmial.com>
2023-06-12 19:45:26 +0530

    code pushes

---
+++ src/test/java/com/example/Yatra/YatraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Yatra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
